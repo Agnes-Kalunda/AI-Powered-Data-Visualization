ab71e4c6940faa844fdb888ca88b49af8fdfe31c
Agnes <agypeter97@gmail.com>
2024-08-10 23:15:13 +0300

    models

--- backend/backendApp/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import DataVisualizationAPIView
-
-urlpatterns = [
-    path('visualize/', DataVisualizationAPIView.as_view(), name='visualize-data'),
-]
--- backend/backendApp/models.py
+++ backend/backendApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Dataset(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    file = models.FileField(upload_to='datasets/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+class DataPoint(models.Model):
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE)
+    x = models.FloatField()
+    y = models.FloatField()
