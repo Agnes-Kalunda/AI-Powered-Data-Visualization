25788b08c00065f4bb269576dfcd234541ca35a3
Agnes <agypeter97@gmail.com>
2024-08-12 20:42:34 +0300

    Register models in admin file

--- backend/backendApp/admin.py
+++ backend/backendApp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Dataset, DataPoint
 
-# Register your models here.
+@admin.register(Dataset)
+class DatasetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'uploaded_at')
+    list_filter = ('user', 'uploaded_at')
+    search_fields = ('name', 'user__username')
+
+@admin.register(DataPoint)
+class DataPointAdmin(admin.ModelAdmin):
+    list_display = ('dataset', 'x', 'y')
+    list_filter = ('dataset',)
+    search_fields = ('dataset__name',)
