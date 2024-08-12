5164f019e84b815c6e3c6d1a06d584db10b61e95
Agnes <agypeter97@gmail.com>
2024-08-12 23:34:24 +0300

    function to display list of datasets uploaded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import DatasetUpload from './components/DatasetUpload';
+import DatasetList from './components/DatasetList';
 
 function App() {
   return (
     <div className="App">
 <DatasetUpload/>
+<DatasetList/>
     </div>
   );
 }
