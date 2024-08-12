39ca8cc01f2b550d4eecae800ccdacccb37261f8
Agnes <agypeter97@gmail.com>
2024-08-12 20:13:02 +0300

    serializers for data conversion

--- backend/backendApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import home
-
-urlpatterns = [
-    # Other URL patterns
-    path('', home, name='home'), 
-    path('api/', include('backendApp.api.urls')),
-]
--- backend/backendApp/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-def home(request):
-    if request.method == 'GET':
-        return HttpResponse("Welcome to the homepage!")
-    return HttpResponse("Method Not Allowed", status=405)  # Handle other methods
